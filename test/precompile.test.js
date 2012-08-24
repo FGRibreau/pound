79ac49b4716e59882db2208c2b5029cff4cf95ba
FG Ribreau <github@fgribreau.com>
2012-08-24 21:25:53 +0100

    Bump to 1.0.0. - [New] pound.precompile();

---
+++ test/precompile.test.js
@@ -0,0 +1,26 @@
+var pound = require('../');
+var defineAsset = null;
+
+module.exports = {
+  setUp: function(callback) {
+    this.P = pound.create();
+    defineAsset = this.P.defineAsset;
+    callback();
+  },
+
+  tearDown: function(callback) {
+    callback();
+  },
+
+  precompile: function(t) {
+    defineAsset({ name: 'app'}, {
+      js: [__dirname+'/precompile.test.js', __dirname+'/pound.test.js']
+    });
+
+    t.doesNotThrow(function(){
+      this.P.precompile();
+    }.bind(this));
+
+    t.done();
+  }
+};
